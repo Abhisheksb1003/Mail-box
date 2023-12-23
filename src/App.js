5824e1b7c974d1c0ede2070e5af6fcbd334c7257
Abhisheksb1003 <abhishekbikkannavar1@gmail.com>
2023-12-23 21:40:00 +0530

    Login Screen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import Headers from './Components/UI/Header';
 import AuthPage from './Components/Pages/Auth-page';
 import './App.css';
 import { Fragment } from "react";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import Home from './Components/Pages/Home';
 
 function App() {
   return (
     <Fragment>
       <Headers />
-      <AuthPage />
+      <Switch>
+        <Route path="/Auth">
+          <AuthPage />
+        </Route>
+        <Route path="/Home">
+          <Home />
+        </Route>
+      </Switch>
     </Fragment>
   );
 }
